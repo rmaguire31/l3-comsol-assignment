98a8b532a347f1fbc22d436e056392ce52ad05e5
rmaguire31 <rmaguire31@gmail.com>
2017-11-20 00:36:34 +0000

    big old update. all the results, finish line in sight

--- matlab/characterise_results.m
+++ matlab/characterise_results.m
@@ -9,43 +9,46 @@ function characterise_results(fname)
 %
 %COPYRIGHT (C) Russell Maguire 2017
 [paramv, Tv, C, dz, cte, paramname] = parse_fname(fname);
-Tname = 'Temperature (\si{\celsius})';
-Cname = 'Capacitance (\si{\femto\farad})';
-dzname = 'Displacement (\si{\micro\meter})';
-ctename = 'Coefficient of thermal expansion (\si{\per\kelvin})';
-stressname = '\ce{SiO2} pre-stress (\si\{\giga\pascal})';
-if strcmp(fname, 'material_study.txt')
-    paramname = ctename;
-    paramv = cte(1,:);
-elseif strcmp(fname, 'prestress_study.txt')
-    Tname = stressname;
+fname = fname(1:end-4);
+
+Tname = 'Temperature \si{\celsius}';
+Cname = 'Capacitance \si{\femto\farad}';
+dzname = 'Displacement \si{\micro\meter}';
+if strcmp(fname, 'material_study')
+    paramname = 'Thermal expansion coefficient \si\{\per\kelvin}';
+    material = {'\ce{Au}', '\ce{Al}', '\ce{Ti}'};
+    paramv = arrayfun(...
+        @(i,x)[material{i} num2str(x,'\\tiny($\\alpha=\\SI{%g}{\\per\\kelvin}$)')], ...
+        paramv, cte(1,:)', 'UniformOutput', false);
 end
 
 % Maximum temperature when the value stays the same. Set these to NaN
 C(isnan(dz)) = nan;
-for i = 1:length(paramv)
-    j0(i) = find(abs(dz(:,i))==min(abs(dz(:,i))));
-end
 plot_tikz(Tv, C, paramv, Tname, Cname, paramname, [fname '.C-T.tex']);
 plot_tikz(Tv, dz, paramv, Tname, dzname, paramname, [fname '.dz-T.tex']);
 plot_tikz(dz, C, paramv, dzname, Cname, paramname, [fname '.C-dz.tex']);
 
 % Get p-values for linearity of C-T. We should be able to identify when
 % the relationship becomes significantly non-linear.
-    function [p, F, b] = regression(x,y)
-        p = zeros(length(x),length(x),length(paramv));
-        F = zeros(length(x),length(x),length(paramv));
-        b = cell(length(x),length(x),length(paramv));
-        for ii = 1:length(paramv)
-            for jj = 1:length(x)
-                for kk = jj:length(x)
-                    if any(isnan(y(jj:kk,ii)))
+    function [p, F, b] = regression(X,Y)
+        p = zeros(length(X),length(X),size(Y,2));
+        F = zeros(length(X),length(X),size(Y,2));
+        b = cell(length(X),length(X),size(Y,2));
+        for ii = 1:size(Y,2)
+            for jj = 1:length(X)
+                for kk = jj:length(X)
+                    if any(isnan(Y(jj:kk,ii)))
                         p([kk,jj],[jj,kk],ii) = nan;
                         continue
                     end
+                    if isvector(X)
+                        x = reshape(X(jj:kk), [], 1);
+                    else
+                        x = X(jj:kk,ii);
+                    end
                     % Perform regression to get p-values out
                     [beta,~,~,stats] = regress(...
-                          x(jj:kk)', [ones(1+kk-jj,1) y(jj:kk,ii)]);
+                          Y(jj:kk,ii), [ones(1+kk-jj,1) x]);
                     b([kk,jj],[jj,kk],ii) = {beta};
                     F([kk,jj],[jj,kk],ii) = stats(end-1);
                     p([kk,jj],[jj,kk],ii) = stats(end);
@@ -55,17 +58,20 @@ plot_tikz(dz, C, paramv, dzname, Cname, paramname, [fname '.C-dz.tex']);
     end
 [p_CT, F_CT, b_CT] = regression(Tv,C);
 [p_dzT, F_dzT, b_dzT] = regression(Tv,dz);
+[p_Cdz, F_Cdz, b_Cdz] = regression(dz(:),C(:));
 
 disp('C-T')
 disp(p_CT)
 disp('dz-T')
 disp(p_dzT)
+disp('C-dz')
+disp(p_Cdz)
 
 % Find linear region
     function lin = linear_region(p)
         alpha = 0.05;
-        lin = cell(length(paramv),2);
-        for ii = 1:length(paramv)
+        lin = cell(size(p,3),3);
+        for ii = 1:size(p,3)
             % Default linear region
             lin(ii,:) = {1, 1, ii};
             brake = false;
@@ -85,59 +91,75 @@ disp(p_dzT)
     end
 lin_CT = linear_region(p_CT);
 lin_dzT = linear_region(p_dzT);
+lin_Cdz = linear_region(p_Cdz);
 
 disp('C-T')
 disp(lin_CT)
 disp('dz-T')
 disp(lin_dzT)
+disp('C-dz')
+disp(lin_Cdz)
 
-% Calculate the sensitivity in the linear region using regression
+% Calculate the sensitivity in the linear region using our regression
+% matrix
     function S = sensitivity(lin, b)
-        S = ones(length(paramv),1);
-        for ii = 1:length(paramv)
-            S(ii) = b{lin{ii,:}};
+        S = ones(size(lin,1),1);
+        for ii = 1:size(lin,1)
+            beta = b{lin{ii,:}};
+            S(ii) = beta(end);
         end
     end
 S_CT = sensitivity(lin_CT, b_CT);
 S_dzT = sensitivity(lin_dzT, b_dzT);
+S_Cdz = sensitivity(lin_Cdz, b_Cdz);
 
 disp('C-T')
 disp(S_CT)
 disp('dz-T')
 disp(S_dzT)
+disp('C-dz')
+disp(S_Cdz)
 
-return
-plot_tikz(Tv, p_CT, paramv, Tname, 'p for C--T', paramname,...
-    [fname '.linearity_C-T.tex'], @plot_alpha_cb);
-plot_tikz(Tv, p_dzT, paramv, Tname, 'p for dz--T', paramname,...
-    [fname '.linearity_dz-T.tex'], @plot_alpha_cb);
-plot_tikz(dz, p_Cdz, paramv, dzname, 'p for C--dz', paramname,...
-    [fname '.linearity_C-dz.tex'], @plot_alpha_cb);
-% Callback to plot alpha level
-    function plot_alpha_cb()
+
+% Get points for the best fit line in the linear region using our 
+% regression matrix
+    function [x, y] = bestfit(lin, b, X)
+        x = ones(2,size(lin,1));
+        y = ones(2,size(lin,1));
+        for ii = 1:size(lin,1)
+            if isvector(X)
+                [kk,jj] = lin{ii,1:2};
+                x(:,ii) = X([jj kk]);
+            else
+                [kk,jj] = lin{ii,1:2};
+                x(:,ii) = X([jj kk], ii);
+            end
+            beta = b{lin{ii,:}};
+            y(:,ii) = [ones(2,1) x(:,ii)]*beta;
+        end
+    end
+[x_CT,y_CT] = bestfit(lin_CT, b_CT, Tv);
+[x_dzT,y_dzT] = bestfit(lin_dzT, b_dzT, Tv);
+[x_Cdz,y_Cdz] = bestfit(lin_Cdz, b_Cdz, dz(:));
+
+plot_tikz(Tv, C, paramv, Tname, Cname, paramname,...
+     [fname '.linearity_C-T.tex'], @()plot_bestfit_cb(x_CT,y_CT));
+plot_tikz(Tv, dz, paramv, Tname, dzname, paramname,...
+    [fname '.linearity_dz-T.tex'], @()plot_bestfit_cb(x_dzT,y_dzT));
+[~,idx] = sort(dz(:));
+plot_tikz(dz(idx), C(idx), {}, dzname, Cname, '',...
+    [fname '.linearity_C-dz.tex'], @()plot_bestfit_cb(x_Cdz,y_Cdz));
+    function plot_bestfit_cb(x,y)
         h = ishold();
         hold('on');
-        plot(Tv, alpha*ones(size(Tv)), '--k')
-        ax = gca();
-        if ~ismember(alpha, ax.YTick)
-            ax.YTick = sort([ax.YTick alpha]);
+        for ii = 1:size(x,2)
+            p = plot(x(:,ii), y(:,ii), '--');
+            p.LineWidth = 4*p.LineWidth;
         end
-        ax.YTickLabel{ax.YTick==0.05} = '(\alpha) 0.05';
         if ~h
             hold('off')
         end
     end
-
-% Compute sensitivity (gradient) of linear region
-for i = 1:length(paramv)
-end
-plot_tikz(paramv, S_CT, 'C-T sensitivity', {}, paramname, yname, '',...
-    [fname 'sensitivity_C-T.tex']);
-plot_tikz(paramv, S_dzT, 'dz-T sensitivity', {}, paramname, yname, '',...
-    [fname 'sensitivity_dz-T.tex']);
-plot_tikz(paramv, S_Cdz, 'C-dz sensitivity', {}, paramname, yname, '',...
-    [fname 'sensitivity_C-dz.tex']);
-
 end
 
 function [paramv, Tv, C, dz, cte, paramname] = parse_fname(fname)
@@ -186,9 +208,18 @@ for i = 2:size(data,1)
     end
 end
 paramv = data(1:length(Tv):end,1);
-C = reshape(data(:,3), length(Tv), length(paramv));
-dz = reshape(data(:,4), length(Tv), length(paramv));
-cte = reshape(data(:,5), length(Tv), length(paramv));
+% Two-fold symmetry => 4*C
+C = 4*reshape(data(:,3), length(Tv), length(paramv));
+if (size(data,2) >= 4)
+    dz = reshape(data(:,4), length(Tv), length(paramv));
+else
+    dz = C;
+end
+if (size(data,2) >= 4)
+    cte = reshape(data(:,5), length(Tv), length(paramv));
+else
+    cte = dz;
+end
 end
 
 function plot_tikz(x, y, paramv, xname, yname, paramname, fname, cb)
@@ -197,19 +228,34 @@ function plot_tikz(x, y, paramv, xname, yname, paramname, fname, cb)
 %
 %INPUTS
 %
+markers = {
+    '-+','-o','-^','-s','-p','-h','-v','-d'};
 figure()
-plot(x, y);
+hold('on');
+for i = 1:size(y,2)
+    if isvector(x)
+        plot(x, y(:,i), markers{i});
+    else
+        plot(x(:,i), y(:,i), markers{i});
+    end
+end
+hold('off');
+
 if ~isvector(y)
     if isnumeric(paramv)
-        paramv = arrayfun(@(x)num2str(x, [paramname ' %g']), paramv,...
+        paramv = arrayfun(@(x)[paramname num2str(x,' %g')], paramv,...
                           'UniformOutput', false);
     end
-    legend(paramv);
+   legend(paramv, 'AutoUpdate', 'off', 'Location', 'northoutside');
 end
 ylabel(yname);
 xlabel(xname);
+
+% Callback if we need to add anything else.
 if exist('cb', 'var')
     cb()
 end
-matlab2tikz(fname);
+matlab2tikz(['tex/tikz/' fname], ...
+            'parseStrings', false, ...
+            'width', '0.4\textwidth');
 end
